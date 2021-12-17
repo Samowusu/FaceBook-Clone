4722c79897a70b1769abeabb106176cf482c413d
Samuel Baah Owusu <samuelowusu958@gmail.com>
2021-12-17 13:27:51 -0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+ apiKey: "AIzaSyDAlAvsGFHb9Z-AKVQ1eQ1IkAj1kFVJ8G4",
+ authDomain: "facebook-clone-8d4e9.firebaseapp.com",
+ projectId: "facebook-clone-8d4e9",
+ storageBucket: "facebook-clone-8d4e9.appspot.com",
+ messagingSenderId: "453583882213",
+ appId: "1:453583882213:web:4f808a3aae54642be6d10c",
+};
